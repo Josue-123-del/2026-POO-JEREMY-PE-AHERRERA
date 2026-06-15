166a05ddfa153c044d69c5162e9a8b3f15e7ae5d
jeremy peñaherrera <jj.penaherrera@uea.edu.ec>
2026-06-14 21:46:28 -0500

    tarea semana#3

---
+++ Programacion-POO/main.py
@@ -0,0 +1,11 @@
+from mascota import Mascota
+
+mascota1 = Mascota("Chester", "Perro", 2)
+mascota2 = Mascota("Oreo", "Gato", 1)
+
+mascota1.mostrar_informacion()
+mascota1.hacer_sonido()
+
+mascota2.mostrar_informacion()
+mascota2.hacer_sonido()
+
